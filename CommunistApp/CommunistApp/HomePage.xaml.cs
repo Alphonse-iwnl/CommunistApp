91da9c0ab6aee819ac6b1917598d644506b78d1e
杨彬 <z1479763604@hotmail.com>
2016-06-16 14:26:02 +0800

    轮播图+新闻页 帖子没做 详情没做

--- CommunistApp/CommunistApp/HomePage.xaml.cs
+++ CommunistApp/CommunistApp/HomePage.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -11,6 +17,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
@@ -35,8 +42,29 @@ namespace CommunistApp
         {
             this.InitializeComponent();
             SixItem.ItemsSource = sixItem;
+            GetPic();
+            this.fvCenter.ItemsSource = PicData;
         }
+        string tempString;
+        List<Pic> PicData = new List<Pic>();
+        
+        void GetPic()
+        {
+            HttpClient httpClient1 = new HttpClient();
+            string uri = "http://202.202.43.42/lxyz/index.php?m=Home&c=index&a=mobilepic";            
+            System.Net.Http.HttpResponseMessage response;
+            response = httpClient1.GetAsync(new Uri(uri)).Result;
+            if (response.StatusCode == HttpStatusCode.OK)
+                tempString = response.Content.ReadAsStringAsync().Result;
 
+            JObject jArray2 = (JObject)JsonConvert.DeserializeObject(tempString);
+            string json2 = jArray2["data"].ToString();
+            JArray jArray = (JArray)JsonConvert.DeserializeObject(json2);
+
+            PicData = JsonConvert.DeserializeObject<List<Pic>>(jArray.ToString());
+            
+        }
+        
         private void SixItem_ItemClick(object sender, ItemClickEventArgs e)
         {
 
@@ -44,6 +72,12 @@ namespace CommunistApp
         public class Item
         {
             public string ItemName { get; set; }
+        }
+        public class Pic
+        {
+            public string title { get; set; }
+            public string link { get; set; }
+            public string imgurl { get; set; }
         }
     }
 }
--- CommunistApp/CommunistApp/NewsPage.xaml.cs
+++ CommunistApp/CommunistApp/NewsPage.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -22,9 +26,57 @@ namespace CommunistApp
     /// </summary>
     public sealed partial class NewsPage : Page
     {
+        List<KeyValuePair<String, String>> paramList = new List<KeyValuePair<String, String>>();
         public NewsPage()
         {
             this.InitializeComponent();
+            GetAllListData();
+        }
+        string tempString;
+        void GetAllListData()
+        {
+            GetNewsData("2");
+            GetNewsData("3");
+            GetNewsData("4");
+            GetNewsData("5");
+        }
+        void GetNewsData(string id)
+        {
+            paramList.Add(new KeyValuePair<string, string>("id", id));
+            HttpClient httpClient = new HttpClient();
+            string uri = $"http://202.202.43.42/lxyz/index.php?m=Home&c=index&a=mobilearticlelist";
+            System.Net.Http.HttpResponseMessage response;
+            response = httpClient.PostAsync(new Uri(uri), new FormUrlEncodedContent(paramList)).Result;
+            if (response.StatusCode == HttpStatusCode.OK)
+                tempString = response.Content.ReadAsStringAsync().Result;
+
+            tempString.Replace("\r\n\r\n", " ");
+            tempString.Replace("&ldquo;", " ");
+            tempString.Replace("&rdquo;", " ");
+            tempString.Replace("&rdq", " ");
+
+            JObject jArray2 = (JObject)JsonConvert.DeserializeObject(tempString);
+            string json2 = jArray2["data"].ToString();
+            JArray jArray = (JArray)JsonConvert.DeserializeObject(json2);
+
+            List<NewsContent> newsContent = JsonConvert.DeserializeObject<List<NewsContent>>(jArray.ToString());
+
+            switch (id)
+            {
+                case "2":
+                    NewsList.ItemsSource = newsContent;
+                    break;
+                case "3":
+                    WorkList.ItemsSource = newsContent;
+                    break;
+                case "4":
+                    MoveList.ItemsSource = newsContent;
+                    break;
+                case "5":
+                    StudyList.ItemsSource = newsContent;
+                    break;
+            }
+
         }
 
         private void NewsList_ItemClick(object sender, ItemClickEventArgs e)
@@ -46,5 +98,13 @@ namespace CommunistApp
         {
 
         }
+        public class NewsContent
+        {
+            public string id { get; set; }
+            public string title { get; set; }
+            public string content { get; set; }
+            public string time { get; set; }
+        }
+
     }
 }
